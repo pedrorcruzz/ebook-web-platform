ec9344513772bf758c716cb0323ce481bd499de1
pedrorcruzz <phrosa.dev@gmail.com>
2025-04-23 10:04:02 -0300

    feat: add price

--- database/migrations/2025_04_22_163657_book.php
+++ database/migrations/2025_04_22_163657_book.php
@@ -10,14 +10,16 @@ return new class extends Migration
     {
         Schema::create('book', function (Blueprint $table) {
             $table->id();
+            $table->enum('status', ['available', 'checked_out'])->default('available');
             $table->unsignedBigInteger('user_author_id');
             $table->string('title', 255);
             $table->string('genre', 100);
             $table->string('isbn', 13)->unique();
+            $table->float('price');
+            $table->integer('pages')->nullable();
             $table->date('publication_date');
             $table->text('description')->nullable();
             $table->string('cover_image')->nullable();
-            $table->enum('status', ['available', 'checked_out'])->default('available');
             $table->timestamps();
 
             $table->foreign('user_author_id')->references('id')->on('user_author')->onDelete('cascade');
